81c4dc7708c0f009f09c0d349ab299a4bf5a721a
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2023-02-20 10:53:41 -0500

    draw map 1

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ const calculate = () => {
 };
 
 const draw = () => {
+  // water
+  ctx.fillStyle = '#4C5CA9';
+  ctx.fillRect(100, 400, 50, 50);
+  ctx.fillRect(0, 450, 50, 50);
+  ctx.fillRect(0, 550, 50, 50);
+  ctx.fillRect(0, 600, 50, 50);
+  ctx.fillRect(450, 400, 50, 50);
+  ctx.fillRect(500, 400, 50, 50);
+  ctx.fillRect(600, 400, 50, 50);
+  ctx.fillRect(600, 450, 50, 50);
+  ctx.fillRect(600, 500, 50, 50);
+  ctx.fillRect(600, 550, 50, 50);
   // do something
   ctx.fillStyle = 'red';
   ctx.fillRect(0, 0, 50, 50);
