f1f82a96bf2d0dd5d1588f6c509840c6e46b2874
RadiCM <fb.conis@gmail.com>
2023-02-20 13:09:03 -0500

    canvas en html

--- main.js
+++ main.js
@@ -4,23 +4,138 @@ const ctx = canvas.getContext('2d');
 canvas.width = 650;
 canvas.height = 650;
 
+class Player {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.step = 25;
+    this.isCollision = false;
+    document.addEventListener('keydown', e => {
+      this.move(e);
+    });
+  }
+  draw() {
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+  move(e) {
+    if (this.isCollision) {
+      return;
+    }
+    switch (e.key) {
+      case 'w':
+        this.y -= this.step;
+        break;
+      case 's':
+        this.y += this.step;
+        break;
+      case 'a':
+        this.x -= this.step;
+        break;
+      case 'd':
+        this.x += this.step;
+        break;
+    }
+  }
+  collision() {
+    this.isCollision = true;
+  }
+}
+
+class Block {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = '#fff';
+    this.isCollision = false;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+}
+
+class Grass extends Block {
+  constructor(x, y, w, h) {
+    super(x, y, w, h);
+    this.color = '#85C441';
+  }
+}
+
+class Water extends Block {
+  constructor(x, y, w, h) {
+    super(x, y, w, h);
+    this.color = '#4C5CA9';
+  }
+  withCollision(player) {
+    if (
+      collision(
+        {
+          x: player.x + player.step,
+          y: player.y + player.step,
+          w: player.w,
+          h: player.h
+        },
+        { x: this.x, y: this.y, w: this.w, h: this.h }
+      )
+    ) {
+      this.isCollision = true;
+      player.collision();
+    }
+  }
+}
+
+const Radi = new Player(200, 600, 50, 50);
+const grass1 = new Grass(200, 300, 50, 50);
+const water1 = new Water(300, 300, 50, 50);
 //player
-let px = 200;
-let py = 600;
-let pw = 50;
-let ph = 50;
+// let px = 200;
+// let py = 600;
+// let pw = 50;
+// let ph = 50;
 
 const calculate = () => {
   // do something
-  if (
-    collision({ x: px, y: py, w: pw, h: ph }, { x: 200, y: 200, w: 50, h: 50 })
-  ) {
-    console.log('collision');
-  }
+  water1.withCollision(Radi);
+  // if (
+  //   collision({ x: px, y: py, w: pw, h: ph }, { x: 200, y: 200, w: 50, h: 50 })
+  // ) {
+  //   console.log('collision');
+  // }
 };
 
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  printMap();
+  Radi.draw();
+  grass1.draw();
+  water1.draw();
+};
+
+const collision = (obj1, obj2) => {
+  let collision = false;
+  if (
+    obj1.x + obj1.w > obj2.x &&
+    obj1.x < obj2.x + obj2.w &&
+    obj1.y + obj1.h > obj2.y &&
+    obj1.y < obj2.y + obj2.h
+  ) {
+    collision = true;
+  }
+  return collision;
+};
+
+const loop = () => {
+  calculate();
+  draw();
+  window.requestAnimationFrame(loop);
+};
+
+const printMap = () => {
   // // water
   // ctx.fillStyle = '#4C5CA9';
   // ctx.fillRect(100, 400, 50, 50);
@@ -33,7 +148,6 @@ const draw = () => {
   // ctx.fillRect(600, 450, 50, 50);
   // ctx.fillRect(600, 500, 50, 50);
   // ctx.fillRect(600, 550, 50, 50);
-
   // // grass
   // ctx.fillStyle = '#85C441';
   // ctx.fillRect(0, 50, 50, 50);
@@ -52,7 +166,6 @@ const draw = () => {
   // ctx.fillRect(500, 200, 50, 50);
   // ctx.fillRect(500, 250, 50, 50);
   // ctx.fillRect(500, 300, 50, 50);
-
   // //brick
   // ctx.fillStyle = '#974B22';
   // ctx.fillRect(50, 50, 50, 50);
@@ -111,7 +224,6 @@ const draw = () => {
   // ctx.fillRect(500, 600, 50, 50);
   // ctx.fillRect(450, 600, 50, 50);
   // ctx.fillRect(450, 550, 50, 50);
-
   // //brick
   // ctx.fillStyle = '#ACABAC';
   // ctx.fillRect(150, 0, 50, 50);
@@ -128,51 +240,7 @@ const draw = () => {
   // ctx.fillRect(600, 150, 50, 50);
   // ctx.fillRect(600, 200, 50, 50);
   // ctx.fillRect(500, 450, 50, 50);
-
-  //player
-  ctx.fillStyle = 'red';
-  ctx.fillRect(px, py, pw, ph);
-
-  //objetopachocar
-  ctx.fillStyle = 'blue';
-  ctx.fillRect(200, 200, pw, ph);
 };
-
-const collision = (obj1, obj2) => {
-  let collision = false;
-  if (
-    obj1.x + obj1.w > obj2.x &&
-    obj1.x < obj2.x + obj2.w &&
-    obj1.y + obj1.h > obj2.h &&
-    obj1.y < obj2.y + obj2.h
-  ) {
-    collision = true;
-  }
-  return collision;
-};
-
-const loop = () => {
-  calculate();
-  draw();
-  window.requestAnimationFrame(loop);
-};
-
-document.addEventListener('keydown', e => {
-  switch (e.key) {
-    case 'w':
-      py -= 25;
-      break;
-    case 's':
-      py += 25;
-      break;
-    case 'a':
-      px -= 25;
-      break;
-    case 'd':
-      px += 25;
-      break;
-  }
-});
 
 loop();
 
