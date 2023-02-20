fb31768c6e5be23d0cc702fa17ea52ddeba4ddd3
RadiCM <fb.conis@gmail.com>
2023-02-20 15:57:30 -0500

    Collision nice

--- main.js
+++ main.js
@@ -12,47 +12,55 @@ class Player {
     this.vy = y;
     this.w = w;
     this.h = h;
-    this.step = 5;
+    this.step = 25;
     this.isCollision = false;
+    this.upDown = false;
     document.addEventListener('keydown', e => {
       this.move(e);
     });
+    document.addEventListener('keyup', e => {
+      this.reset();
+    });
   }
   draw() {
+    if (!this.isCollision) {
+      this.x = this.vx;
+      this.y = this.vy;
+    }
     ctx.fillStyle = 'red';
     ctx.fillRect(this.x, this.y, this.w, this.h);
   }
   move(e) {
-    if (this.isCollision) {
-      return;
+    if (e.key === 'w' || e.key === 's') {
+      this.upDown = true;
+    } else {
+      this.upDown = false;
     }
-    this.x = this.vx;
-    this.y = this.vy;
-    switch (e.key) {
-      case 'w':
+    if (this.upDown) {
+      if (e.key == 'w') {
         this.vy = this.y - this.step;
-        break;
-      case 's':
+      }
+      if (e.key == 's') {
         this.vy = this.y + this.step;
-        break;
-      case 'a':
+      }
+    } else {
+      if (e.key == 'a') {
         this.vx = this.x - this.step;
-        break;
-      case 'd':
+      }
+      if (e.key == 'd') {
         this.vx = this.x + this.step;
-        break;
+      }
     }
   }
   collision(block) {
-    this.isCollision = false;
     if (collision({ x: this.vx, y: this.vy, w: this.w, h: this.h }, block)) {
       this.isCollision = true;
-      // this.vx = this.x;
-      // this.vy = this.y;
-    } else {
-      // this.x = this.vx;
-      // this.y = this.vy;
     }
+  }
+  reset() {
+    this.isCollision = false;
+    this.vx = this.x;
+    this.vy = this.y;
   }
 }
 
@@ -72,55 +80,74 @@ class Block {
 }
 
 class Grass extends Block {
-  constructor(x, y, w, h) {
+  constructor({ x, y, w, h }) {
     super(x, y, w, h);
     this.color = '#85C441';
   }
 }
 
 class Water extends Block {
-  constructor(x, y, w, h) {
+  constructor({ x, y, w, h }) {
     super(x, y, w, h);
     this.color = '#4C5CA9';
   }
 }
 
 class Metal extends Block {
-  constructor(x, y, w, h) {
+  constructor({ x, y, w, h }) {
     super(x, y, w, h);
     this.color = '#ACABAC';
   }
 }
 
+class Brick extends Block {
+  constructor({ x, y, w, h }) {
+    super(x, y, w, h);
+    this.color = '#974B22';
+  }
+}
+
+let waters = [];
+for (let i = 0; i < watersPosition.length; i++) {
+  waters.push(new Water(watersPosition[i]));
+}
+
+let bricks = [];
+for (let i = 0; i < bricksPosition.length; i++) {
+  bricks.push(new Brick(bricksPosition[i]));
+}
+
+let metals = [];
+for (let i = 0; i < metalsPosition.length; i++) {
+  metals.push(new Metal(metalsPosition[i]));
+}
+
+let grasses = [];
+for (let i = 0; i < grassesPosition.length; i++) {
+  grasses.push(new Grass(grassesPosition[i]));
+}
+
+console.log(waters);
 const Radi = new Player(200, 600, 50, 50);
 const grass1 = new Grass(200, 300, 50, 50);
 const metal1 = new Metal(300, 300, 50, 50);
 const metal2 = new Metal(500, 300, 50, 50);
 const water1 = new Water(400, 300, 50, 50);
-//player
-// let px = 200;
-// let py = 600;
-// let pw = 50;
-// let ph = 50;
 
 const calculate = () => {
-  // do something
-  Radi.collision(metal1);
-  Radi.collision(water1);
-  // if (
-  //   collision({ x: px, y: py, w: pw, h: ph }, { x: 200, y: 200, w: 50, h: 50 })
-  // ) {
-  //   console.log('collision');
-  // }
+  waters.map(elem => Radi.collision(elem));
+  bricks.map(elem => Radi.collision(elem));
+  metals.map(elem => Radi.collision(elem));
 };
 
 const draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  printMap();
   Radi.draw();
-  grass1.draw();
-  water1.draw();
-  metal1.draw();
+
+  waters.map(elem => elem.draw());
+  bricks.map(elem => elem.draw());
+  grasses.map(elem => elem.draw());
+  metals.map(elem => elem.draw());
 };
 
 const collision = (obj1, obj2) => {
@@ -142,113 +169,4 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 
-const printMap = () => {
-  // // water
-  // ctx.fillStyle = '#4C5CA9';
-  // ctx.fillRect(100, 400, 50, 50);
-  // ctx.fillRect(0, 450, 50, 50);
-  // ctx.fillRect(0, 550, 50, 50);
-  // ctx.fillRect(0, 600, 50, 50);
-  // ctx.fillRect(450, 400, 50, 50);
-  // ctx.fillRect(500, 400, 50, 50);
-  // ctx.fillRect(600, 400, 50, 50);
-  // ctx.fillRect(600, 450, 50, 50);
-  // ctx.fillRect(600, 500, 50, 50);
-  // ctx.fillRect(600, 550, 50, 50);
-  // // grass
-  // ctx.fillStyle = '#85C441';
-  // ctx.fillRect(0, 50, 50, 50);
-  // ctx.fillRect(0, 100, 50, 50);
-  // ctx.fillRect(0, 150, 50, 50);
-  // ctx.fillRect(0, 200, 50, 50);
-  // ctx.fillRect(0, 250, 50, 50);
-  // ctx.fillRect(50, 250, 50, 50);
-  // ctx.fillRect(150, 100, 50, 50);
-  // ctx.fillRect(250, 100, 50, 50);
-  // ctx.fillRect(200, 100, 50, 50);
-  // ctx.fillRect(250, 300, 50, 50);
-  // ctx.fillRect(300, 300, 50, 50);
-  // ctx.fillRect(200, 350, 50, 50);
-  // ctx.fillRect(0, 500, 50, 50);
-  // ctx.fillRect(500, 200, 50, 50);
-  // ctx.fillRect(500, 250, 50, 50);
-  // ctx.fillRect(500, 300, 50, 50);
-  // //brick
-  // ctx.fillStyle = '#974B22';
-  // ctx.fillRect(50, 50, 50, 50);
-  // ctx.fillRect(50, 100, 50, 50);
-  // ctx.fillRect(0, 300, 50, 50);
-  // ctx.fillRect(100, 300, 50, 50);
-  // ctx.fillRect(50, 300, 50, 50);
-  // ctx.fillRect(150, 150, 50, 50);
-  // ctx.fillRect(150, 200, 50, 50);
-  // ctx.fillRect(150, 250, 50, 50);
-  // ctx.fillRect(150, 300, 50, 50);
-  // ctx.fillRect(200, 250, 50, 50);
-  // ctx.fillRect(200, 300, 50, 50);
-  // ctx.fillRect(250, 250, 50, 50);
-  // ctx.fillRect(250, 200, 50, 50);
-  // ctx.fillRect(50, 400, 50, 50);
-  // ctx.fillRect(50, 450, 50, 50);
-  // ctx.fillRect(50, 500, 50, 50);
-  // ctx.fillRect(50, 550, 50, 50);
-  // ctx.fillRect(50, 600, 50, 50);
-  // ctx.fillRect(150, 600, 50, 50);
-  // ctx.fillRect(150, 500, 50, 50);
-  // ctx.fillRect(150, 450, 50, 50);
-  // ctx.fillRect(250, 350, 50, 50);
-  // ctx.fillRect(250, 400, 50, 50);
-  // ctx.fillRect(250, 450, 50, 50);
-  // ctx.fillRect(250, 500, 50, 50);
-  // ctx.fillRect(300, 500, 50, 50);
-  // ctx.fillRect(350, 500, 50, 50);
-  // ctx.fillRect(300, 450, 50, 50);
-  // ctx.fillRect(350, 450, 50, 50);
-  // ctx.fillRect(350, 400, 50, 50);
-  // ctx.fillRect(350, 350, 50, 50);
-  // ctx.fillRect(400, 300, 50, 50);
-  // ctx.fillRect(450, 300, 50, 50);
-  // ctx.fillRect(300, 150, 50, 50);
-  // ctx.fillRect(300, 100, 50, 50);
-  // ctx.fillRect(350, 100, 50, 50);
-  // ctx.fillRect(350, 150, 50, 50);
-  // ctx.fillRect(350, 50, 50, 50);
-  // ctx.fillRect(450, 250, 50, 50);
-  // ctx.fillRect(450, 200, 50, 50);
-  // ctx.fillRect(450, 100, 50, 50);
-  // ctx.fillRect(450, 50, 50, 50);
-  // ctx.fillRect(550, 100, 50, 50);
-  // ctx.fillRect(550, 50, 50, 50);
-  // ctx.fillRect(550, 200, 50, 50);
-  // ctx.fillRect(450, 350, 50, 50);
-  // ctx.fillRect(550, 400, 50, 50);
-  // ctx.fillRect(550, 350, 50, 50);
-  // ctx.fillRect(550, 300, 50, 50);
-  // ctx.fillRect(550, 450, 50, 50);
-  // ctx.fillRect(450, 450, 50, 50);
-  // ctx.fillRect(550, 550, 50, 50);
-  // ctx.fillRect(550, 600, 50, 50);
-  // ctx.fillRect(500, 600, 50, 50);
-  // ctx.fillRect(450, 600, 50, 50);
-  // ctx.fillRect(450, 550, 50, 50);
-  // //brick
-  // ctx.fillStyle = '#ACABAC';
-  // ctx.fillRect(150, 0, 50, 50);
-  // ctx.fillRect(150, 50, 50, 50);
-  // ctx.fillRect(150, 350, 50, 50);
-  // ctx.fillRect(150, 400, 50, 50);
-  // ctx.fillRect(0, 400, 50, 50);
-  // ctx.fillRect(350, 300, 50, 50);
-  // ctx.fillRect(400, 250, 50, 50);
-  // ctx.fillRect(300, 200, 50, 50);
-  // ctx.fillRect(350, 0, 50, 50);
-  // ctx.fillRect(450, 150, 50, 50);
-  // ctx.fillRect(500, 100, 50, 50);
-  // ctx.fillRect(600, 150, 50, 50);
-  // ctx.fillRect(600, 200, 50, 50);
-  // ctx.fillRect(500, 450, 50, 50);
-};
-
 loop();
-
-console.log(canvas);
